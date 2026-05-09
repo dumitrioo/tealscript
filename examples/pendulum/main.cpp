7115d105749abe9490b0e5bd6fc730c95acd0ef6
dumitrioo <dmitryy@mailfence.com>
2026-05-09 14:22:47 +0300

    pendulum demo corrections

--- examples/pendulum/main.cpp
+++ examples/pendulum/main.cpp
@@ -6,59 +6,77 @@
 #include <raylib.h>
 #include <rlgl.h>
 
+template<typename T, typename D = std::default_delete<T>>
+using uniqp_t = std::unique_ptr<T, D>;
+
 int main(int argc, char **argv) {
     if(argc < 2) {
         return 0;
     }
+
     teal::runtime teal_rt{};
     teal_rt.load_file(argv[1]);
     teal_rt.loading_complete();
     teal_rt.run_cycle();
 
     // 1. INIT BULLET PHYSICS
-    std::unique_ptr<btDefaultCollisionConfiguration> collisionConfiguration{std::make_unique<btDefaultCollisionConfiguration>()};
-    std::unique_ptr<btCollisionDispatcher> dispatcher{std::make_unique<btCollisionDispatcher>(collisionConfiguration.get())};
-    std::unique_ptr<btBroadphaseInterface> overlappingPairCache{std::make_unique<btDbvtBroadphase>()};
-    std::unique_ptr<btSequentialImpulseConstraintSolver> solver{std::make_unique<btSequentialImpulseConstraintSolver>()};
-    std::unique_ptr<btDiscreteDynamicsWorld> dynamicsWorld{std::make_unique<btDiscreteDynamicsWorld>(dispatcher.get(), overlappingPairCache.get(), solver.get(), collisionConfiguration.get())};
+    uniqp_t<btDefaultCollisionConfiguration> collisionConfiguration{std::make_unique<btDefaultCollisionConfiguration>()};
+    uniqp_t<btCollisionDispatcher> dispatcher{std::make_unique<btCollisionDispatcher>(collisionConfiguration.get())};
+    uniqp_t<btBroadphaseInterface> overlappingPairCache{std::make_unique<btDbvtBroadphase>()};
+    uniqp_t<btSequentialImpulseConstraintSolver> solver{std::make_unique<btSequentialImpulseConstraintSolver>()};
+    uniqp_t<btDiscreteDynamicsWorld> dynamicsWorld{std::make_unique<btDiscreteDynamicsWorld>(
+        dispatcher.get(), overlappingPairCache.get(), solver.get(), collisionConfiguration.get())
+    };
 
     dynamicsWorld->setGravity(btVector3(0, -9.81, 0));
 
     // 2. Ground
-    std::unique_ptr<btCollisionShape> groundShape{std::make_unique<btStaticPlaneShape>(btVector3(0, 1, 0), 0)};
-    std::unique_ptr<btDefaultMotionState> groundMotionState{std::make_unique<btDefaultMotionState>(btTransform(btQuaternion(0, 0, 0, 1), btVector3(0, 0, 0)))};
-    std::unique_ptr<btCollisionObject> groundCollisionObj{std::make_unique<btCollisionObject>()};
+    uniqp_t<btCollisionShape> groundShape{std::make_unique<btStaticPlaneShape>(btVector3(0, 1, 0), 0)};
+    uniqp_t<btDefaultMotionState> groundMotionState{std::make_unique<btDefaultMotionState>(
+        btTransform(btQuaternion(0, 0, 0, 1), btVector3(0, 0, 0)))
+    };
+    uniqp_t<btCollisionObject> groundCollisionObj{std::make_unique<btCollisionObject>()};
     groundCollisionObj->setCollisionShape(groundShape.get());
     groundCollisionObj->setFriction(teal_rt.get_output("friction_force").cast_to_double());
-    btRigidBody::btRigidBodyConstructionInfo groundRigidBodyCI(0, groundMotionState.get(), groundShape.get(), btVector3(0,0,0));
-    std::unique_ptr<btRigidBody> groundBody{std::make_unique<btRigidBody>(groundRigidBodyCI)};
+    btRigidBody::btRigidBodyConstructionInfo groundRigidBodyCI(
+        0, groundMotionState.get(), groundShape.get(), btVector3(0,0,0)
+    );
+    uniqp_t<btRigidBody> groundBody{std::make_unique<btRigidBody>(groundRigidBodyCI)};
     dynamicsWorld->addRigidBody(groundBody.get());
 
     // 3. Cart
-    std::unique_ptr<btCollisionShape> cartShape{std::make_unique<btBoxShape>(btVector3(0.5, 0.15, 0.3))};
-    std::unique_ptr<btCollisionObject> cartCollisionObj{std::make_unique<btCollisionObject>()};
+    uniqp_t<btCollisionShape> cartShape{std::make_unique<btBoxShape>(btVector3(0.5, 0.15, 0.3))};
+    uniqp_t<btCollisionObject> cartCollisionObj{std::make_unique<btCollisionObject>()};
     cartCollisionObj->setCollisionShape(cartShape.get());
     cartCollisionObj->setFriction(teal_rt.get_output("friction_force").cast_to_double());
     btScalar cartMass = teal_rt.get_output("cart_mass").cast_to_double();
     btVector3 cartInertia(0,0,0);
     cartShape->calculateLocalInertia(cartMass, cartInertia);
-    std::unique_ptr<btDefaultMotionState> cartMotionState{std::make_unique<btDefaultMotionState>(btTransform(btQuaternion(0,0,0,1), btVector3(0, 0.15, 0)))};
-    btRigidBody::btRigidBodyConstructionInfo cartRigidBodyCI(cartMass, cartMotionState.get(), cartShape.get(), cartInertia);
-    std::unique_ptr<btRigidBody> cartBody{std::make_unique<btRigidBody>(cartRigidBodyCI)};
+    uniqp_t<btDefaultMotionState> cartMotionState{std::make_unique<btDefaultMotionState>(
+        btTransform(btQuaternion(0,0,0,1), btVector3(0, 0.15, 0)))
+    };
+    btRigidBody::btRigidBodyConstructionInfo cartRigidBodyCI(
+        cartMass, cartMotionState.get(), cartShape.get(), cartInertia
+    );
+    uniqp_t<btRigidBody> cartBody{std::make_unique<btRigidBody>(cartRigidBodyCI)};
     dynamicsWorld->addRigidBody(cartBody.get());
 
     // 4. Pendulum
-    std::unique_ptr<btCollisionShape> poleShape{std::make_unique<btBoxShape>(btVector3(0.005, 1.0, 0.005))};
+    uniqp_t<btCollisionShape> poleShape{std::make_unique<btBoxShape>(btVector3(0.005, 1.0, 0.005))};
     btScalar poleMass = teal_rt.get_output("pend_mass").cast_to_double();
     btVector3 poleInertia(0,0,0);
     poleShape->calculateLocalInertia(poleMass, poleInertia);
-    std::unique_ptr<btDefaultMotionState> poleMotionState{std::make_unique<btDefaultMotionState>(btTransform(btQuaternion(0,0,0,1), btVector3(0, 1.3, 0)))};
-    btRigidBody::btRigidBodyConstructionInfo poleRigidBodyCI(poleMass, poleMotionState.get(), poleShape.get(), poleInertia);
-    std::unique_ptr<btRigidBody> poleBody{std::make_unique<btRigidBody>(poleRigidBodyCI)};
+    uniqp_t<btDefaultMotionState> poleMotionState{std::make_unique<btDefaultMotionState>(
+        btTransform(btQuaternion(0,0,0,1), btVector3(0, 1.3, 0)))
+    };
+    btRigidBody::btRigidBodyConstructionInfo poleRigidBodyCI(
+        poleMass, poleMotionState.get(), poleShape.get(), poleInertia
+    );
+    uniqp_t<btRigidBody> poleBody{std::make_unique<btRigidBody>(poleRigidBodyCI)};
     dynamicsWorld->addRigidBody(poleBody.get());
 
     // 5. Connection
-    std::unique_ptr<btHingeConstraint> poleHinge{std::make_unique<btHingeConstraint>(
+    uniqp_t<btHingeConstraint> poleHinge{std::make_unique<btHingeConstraint>(
         *poleBody,
         *cartBody,
         btVector3(0, -1.0, 0),
@@ -70,7 +88,7 @@ int main(int argc, char **argv) {
     dynamicsWorld->addConstraint(poleHinge.get(), true);
 
     // Cart X-axis Slider
-    std::unique_ptr<btSliderConstraint> cartSlider{std::make_unique<btSliderConstraint>(
+    uniqp_t<btSliderConstraint> cartSlider{std::make_unique<btSliderConstraint>(
         *cartBody, btTransform::getIdentity(), true
     )};
     cartSlider->setLowerLinLimit(-1000000000.0);
@@ -101,7 +119,13 @@ int main(int argc, char **argv) {
             SetConfigFlags(FLAG_MSAA_4X_HINT);
             InitWindow(1280, 720, "TealScript Physics Demo");
             SetTargetFPS(60);
-            Camera3D camera = { { 8.0f, 6.0f, 8.0f }, { 0.0f, 0.5f, 0.0f }, { 0.0f, 1.0f, 0.0f }, 45.0f, 0 };
+            Camera3D camera = {
+                { 8.0f, 6.0f, 8.0f },
+                { 0.0f, 0.5f, 0.0f },
+                { 0.0f, 1.0f, 0.0f },
+                45.0f,
+                0
+            };
 
             while(!term && !WindowShouldClose()) {
                 BeginDrawing();
@@ -112,7 +136,11 @@ int main(int argc, char **argv) {
                 btTransform tCart; cartBody->getMotionState()->getWorldTransform(tCart);
                 btTransform tPole; poleBody->getMotionState()->getWorldTransform(tPole);
                 // Draw Cart
-                Vector3 cPos = { (float)tCart.getOrigin().x(), (float)tCart.getOrigin().y(), (float)tCart.getOrigin().z() };
+                Vector3 cPos = {
+                    (float)tCart.getOrigin().x(),
+                    (float)tCart.getOrigin().y(),
+                    (float)tCart.getOrigin().z()
+                };
                 DrawCubeV(cPos, (Vector3){1.0f, 0.3f, 0.6f}, BLUE);
                 // Draw Pendulum
                 {
@@ -135,10 +163,10 @@ int main(int argc, char **argv) {
                 }
                 // Force vector (Arrow)
                 Vector3 forceStart = { cPos.x, cPos.y, cPos.z + 0.5f };
-                Vector3 forceEnd = { cPos.x + (float)force * 1.0f, cPos.y, cPos.z + 0.5f };
+                Vector3 forceEnd = { cPos.x + (float)force, cPos.y, cPos.z + 0.5f };
                 DrawCylinderEx(forceStart, forceEnd, 0.02, 0.02, 10, MAROON);
                 Vector3 arrowEnd{forceEnd};
-                arrowEnd.x += teal::math::sign(arrowEnd.x) * 0.2;
+                arrowEnd.x += teal::math::sign(force) * 0.2;
                 DrawCylinderEx(forceEnd, arrowEnd, 0.05, 0.0, 10, MAROON);
                 // Soft walls
                 DrawCube((Vector3){4.5f, 0.5f, 0.0f}, 0.1f, 1.0f, 1.0f, Fade(VIOLET, 0.3f));
@@ -146,14 +174,25 @@ int main(int argc, char **argv) {
                 EndMode3D();
                 // 2D overlay
                 int y_draw_pos{10};
-                DrawText(TextFormat("Angle: %0.3f rad", angle), 10, y_draw_pos, 20, DARKGRAY); y_draw_pos += 25;
-                DrawText(TextFormat("Angular speed: %0.3f rad/sec", ang_vel), 10, y_draw_pos, 20, DARKGRAY); y_draw_pos += 25;
-                DrawText(TextFormat("Force: %0.3f N", force), 10, y_draw_pos, 20, DARKGRAY); y_draw_pos += 25;
-                DrawText(TextFormat("Cart Mass: %0.3f kg", cartMass), 10, y_draw_pos, 20, DARKGRAY); y_draw_pos += 25;
-                DrawText(TextFormat("Pendulum Mass: %0.3f kg", poleMass), 10, y_draw_pos, 20, DARKGRAY); y_draw_pos += 25;
-                DrawText(TextFormat("Cart Offset: %0.3f m", cart_x), 10, y_draw_pos, 20, DARKGRAY); y_draw_pos += 25;
-                DrawText(TextFormat("Cart Velocity: %0.3f m/s", cart_vel), 10, y_draw_pos, 20, DARKGRAY); y_draw_pos += 25;
-                DrawText(TextFormat("Cart Friction Coeff: %0.3f", teal_rt.get_output("friction_force").cast_to_double()), 10, y_draw_pos, 20, DARKGRAY); y_draw_pos += 25;
+                DrawText(TextFormat("Angle: %0.3f rad", angle), 10, y_draw_pos, 20, DARKGRAY);
+                y_draw_pos += 25;
+                DrawText(TextFormat("Angular speed: %0.3f rad/sec", ang_vel), 10, y_draw_pos, 20, DARKGRAY);
+                y_draw_pos += 25;
+                DrawText(TextFormat("Force: %0.3f N", force), 10, y_draw_pos, 20, DARKGRAY);
+                y_draw_pos += 25;
+                DrawText(TextFormat("Cart Mass: %0.3f kg", cartMass), 10, y_draw_pos, 20, DARKGRAY);
+                y_draw_pos += 25;
+                DrawText(TextFormat("Pendulum Mass: %0.3f kg", poleMass), 10, y_draw_pos, 20, DARKGRAY);
+                y_draw_pos += 25;
+                DrawText(TextFormat("Cart Offset: %0.3f m", cart_x), 10, y_draw_pos, 20, DARKGRAY);
+                y_draw_pos += 25;
+                DrawText(TextFormat("Cart Velocity: %0.3f m/s", cart_vel), 10, y_draw_pos, 20, DARKGRAY);
+                y_draw_pos += 25;
+                DrawText(TextFormat("Cart Friction Coeff: %0.3f",
+                    teal_rt.get_output("friction_force").cast_to_double()), 10,
+                    y_draw_pos, 20, DARKGRAY
+                );
+                y_draw_pos += 25;
                 EndDrawing();
             }
             CloseWindow();
@@ -161,7 +200,7 @@ int main(int argc, char **argv) {
         }
     };
 
-    for (int64_t i = 0; !should_close; ++i) {
+    for(int64_t i = 0; !should_close; ++i) {
         std::this_thread::sleep_for(std::chrono::duration<double, std::ratio<1, 1>>{sim_cycle_time});
         long double curr_time{teal::steady_time_sec()};
         double dt = curr_time - prev_time;
